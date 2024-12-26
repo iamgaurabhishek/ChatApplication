b3ccc41ec1b5dacd7fa7c634defa365c83f7043f
Abhishek Gaur <abhishekgaur1996@gmail.com>
2024-12-26 08:26:59 -0500

    Created few components for AuthForm and worked on the AuthForm

--- frontEnd/client/src/components/InputCard.js
+++ frontEnd/client/src/components/InputCard.js
@@ -5,13 +5,15 @@ const InputCard = ({
     name = '',
     type = 'text',
     className = '',
-    isRequired = false,
+    isRequired = true,
     placeholder = '',
+    value = '',
+    onChange = () => {},
 }) => {
   return (
     <div className="w-[50%]">
-      <label for={name} className="block text-sm font-medium text-gray-700">{label}</label>
-      <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`} placeholder={placeholder} required={isRequired}/>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-700">{label}</label>
+      <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`} placeholder={placeholder} required={isRequired} value={value} onChange={onChange}/>
     </div>
   )
 }
--- frontEnd/client/src/modules/AuthForm.js
+++ frontEnd/client/src/modules/AuthForm.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputCard from '../components/InputCard'
+import Button from '../components/Button'
 
-const AuthForm = () => {
+const AuthForm = ({isSignInPage = false}) => {
+
+    const [data, setData] = useState({
+        ...(!isSignInPage && {
+            fullName: '',
+        }),
+        email: '',
+        password: '',
+    })
+
+    console.log('data: >> ', data);
+
   return (
     <div className='bg-white w-[35%] h-[87%] shadow-lg rounded-lg flex flex-col justify-center items-center'>
-        <div className='text-4xl font-extrabold'>Welcome</div>
-        <div className='text-xl font-light mb-16'>Sign up now to get started</div>
-        <InputCard label='Full name' name='name' placeholder='Enter your full name' className='mb-6'/>
-        <InputCard label='Email address' type='email' name='email' placeholder='Enter your email id' className='mb-6'/>
-        <InputCard label='Password' type='password' name='password' placeholder='Enter your password' className='mb-6'/>
+        <div className='text-4xl font-extrabold'>Welcome {isSignInPage && 'Back'}</div>
+        <div className='text-xl font-light mb-16'>{isSignInPage ? "Sign In to explore more" : "Sign up now to get started"}</div>
+
+        <form className='flex flex-col items-center w-full' onSubmit={() => console.log('Submitted..')}>
+            {!isSignInPage && <InputCard label='Full name' name='name' placeholder='Enter your full name' className='mb-6' value={data.fullName} onChange={(e) => setData({ ...data, fullName: e.target.value})} />}
+            <InputCard label='Email address' type='email' name='email' placeholder='Enter your email id' className='mb-6' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value})} />
+            <InputCard label='Password' type='password' name='password' placeholder='Enter your password' className='mb-10' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value})} />
+            <Button label={isSignInPage ? 'Sign in' : 'Sign up'} type='submit' className='w-[50%] mb-2'/>
+        </form>
+        <div>{isSignInPage ? "Did not have an account?" :  "Already have an account?"} <span className='text-primary cursor-pointer underline'>{ isSignInPage? "Sign up" : "Sign in"}</span></div>
     </div>
   )
 }
